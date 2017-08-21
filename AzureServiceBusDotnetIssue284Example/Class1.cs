2149a024039bf8d093a3a42f207e3fc135c5d261
Sakher Sawan <eng.sakher@gmail.com>
2017-08-21 06:55:09 +0100

    Init

---
+++ AzureServiceBusDotnetIssue284Example/Class1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusDotnetIssue284Example
+{
+    public class Class1
+    {
+        public async static Task Reproduce()
+        {
+            string azureServiceBusConnectionString = "";
+            string queueName = "";
+
+            IQueueClient queueClient = new QueueClient(azureServiceBusConnectionString
+                , queueName, ReceiveMode.PeekLock);
+            IMessageReceiver rec = new MessageReceiver(azureServiceBusConnectionString
+                , queueName, ReceiveMode.PeekLock);
+
+            await queueClient.SendAsync(new Message(new byte[300]) { MessageId = Guid.NewGuid().ToString() });
+
+
+            queueClient = new QueueClient(azureServiceBusConnectionString
+                , queueName, ReceiveMode.PeekLock);
+            rec = new MessageReceiver(azureServiceBusConnectionString
+                , queueName, ReceiveMode.PeekLock);
+
+
+            var res = await rec.ReceiveAsync();
+
+            queueClient = new QueueClient(azureServiceBusConnectionString
+                , queueName, ReceiveMode.PeekLock);
+            rec = new MessageReceiver(azureServiceBusConnectionString
+                , queueName, ReceiveMode.PeekLock);
+
+
+            await rec.CompleteAsync(res.SystemProperties.LockToken);
+
+        }
+    }
+}
---
+++ AzureServiceBusDotnetIssue284ExampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AzureServiceBusDotnetIssue284ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = AzureServiceBusDotnetIssue284Example.Class1.Reproduce();
+            x.Wait();
+        }
+    }
+}
